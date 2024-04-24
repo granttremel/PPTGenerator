31d53e46d4d2ab2d154b8cf251e688220c739045
granttremel <grant.tremel@gmail.com>
2024-04-24 12:28:26 -0700

    added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='PPTGenerator',
+      description='Generate powerpoints from cosmx image files',
+      author='Grant Tremel',
+      author_email='gtremel@nanostring.com',
+      version='1.0',
+      packages = ['PPTGenerator']      
+#py_modules=['ppt','cosmx_strings','util'],
+      )
